6f7d6b76fd244aed803316aa93361bc272d6839c
Jardel Kenzie <jardel@kenzie.com.br>
2022-09-01 02:34:01 -0300

    fix: removendo teste duplicado

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -8,8 +8,4 @@ router.post("/register/user", CreateUsers.executeUser)
 router.post("/register/admin", CreateUsers.executeAdminUser)
 router.post("/login", AuthenticateUser.execute)
 
-router.get("/test",  (request, response) =>  {
-    return response.status(200).json("we")
-})
-
 export default router
