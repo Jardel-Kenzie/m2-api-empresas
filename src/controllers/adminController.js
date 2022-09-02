6db7343110368987377860270a4eb5643258f664
Jardel Kenzie <jardel@kenzie.com.br>
2022-09-01 23:41:28 -0300

    feat: create request admin

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -1,6 +1,8 @@
 import Company from "../database/models/company.js";
 import Department from "../database/models/department.js";
+import Sector from "../database/models/sector.js";
 import User from "../database/models/user.js";
+import Helper from "../services/helper.js";
 
 export default class AdminController{
     static async getDepartments(request, response){
@@ -10,7 +12,7 @@ export default class AdminController{
     }
 
     static async getOutOfWork(request, response){
-        console.log("iue")
+ 
         const users = await User.findAll({
             where:  {
                 department_uuid: null,
@@ -21,6 +23,28 @@ export default class AdminController{
         return response.status(200).json(users)
     }
 
+    static async getSectors(request, response){
+        const sectors = Sector.findAll()
+
+        return response.status(200).json(sectors)
+    }
+
+
+    static async createSector(request, response){
+        const {description} = request.body
+
+        try{
+
+            const newSector = await Sector.create({
+                description
+            })
+
+            return response.status(201).json(newSector)
+
+        }catch(errors) {
+            return response.status(400).json({ error: Helper.organizationErrors(errors)})
+        }
+    }
 
     static async createCompany(request, response){
 
@@ -41,5 +65,82 @@ export default class AdminController{
         }
     }
 
+    static async createDepartment(request, response){
+        const {name, description, company_uuid} = request.body
+
+        try {
+            const createdDepartment = await Department.create({
+                name,
+                description,
+                company_uuid
+            })
+            
+            return response.status(201).json(createdDepartment)
+    
+        } catch(errors) {
+            return response.status(400).json({ error: Helper.organizationErrors(errors)})
+        }
+    }
+
+    static async createEngage(request, response){
+        const {department_uuid, user_uuid} = request.body
+
+        const user = await User.findByPk(user_uuid)
+
+        if(!user){
+            return response.status(404).json({error: "user not found"})
+        }
+
+        if(user.department_uuid){
+            return response.status(400).json({error: "user alread department!"})
+        }
+
+        const department = await Department.findByPk(department_uuid)
+
+        if(!department){
+            return response.status(404).json({error: "department not found"})
+        }
+
+        try {
+            user.update({
+                department_uuid
+            }, {attributes: {exclude: ['password']}})
+
+            return response.status(200).json(user)
+    
+        } catch(errors) {
+            return response.status(400).json({ error: Helper.organizationErrors(errors)})
+        }
+    }
+
+    static async deleteDepartmentForUser(request, response){
+        const {user_uuid} = request.params
+
+        console.log(request.params)
+
+        const user = await User.findByPk(user_uuid)
+
+        if(!user){
+            return response.status(404).json({error: "user not found"})
+        }
+
+        if(!user.department_uuid){
+            return response.status(400).json({error: "user does not belong to any department"})
+        }
+
+
+        try {
+            user.update({
+                department_uuid: null
+            }, {attributes: {exclude: ['password']}})
+
+            return response.status(200).json(user)
+    
+        } catch(errors) {
+            return response.status(400).json({ error: Helper.organizationErrors(errors)})
+        }
+    }
+
+   
 
 }
