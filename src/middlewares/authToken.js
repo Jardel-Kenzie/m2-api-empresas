2090fbb19a63a52f12e88be324f5b0f4532d9506
Raphael <raphael@kenzie.com.br>
2022-09-01 23:27:24 -0300

    feat: list all users of same department of logged user. List all departments of same company of logged user

--- src/middlewares/authToken.js
+++ src/middlewares/authToken.js
@@ -5,7 +5,6 @@ const { verify } = pkgJwt
 
 export default class AuthToken{
     static async tokenBasic (request, response) {
-
         if(!request.headers.authorization){
             return response.status(401).json({error: "header authorization required"})
         }
@@ -40,6 +39,29 @@ export default class AuthToken{
         if(!user.is_admin){
             return response.status(401).json({error:"need admin permission to access"})
         }
+
+        next()
+    }
+
+    static async hasBasicToken(request, response, next) {
+        if(!request.headers.authorization){
+            return response.status(401).json({error: "header authorization required"})
+        }
+
+        const [, token] = request.headers.authorization.split(" ")
+        
+        if(!token){
+            return response.status(401).json({error: "token is missing!"})
+        }
+
+        
+        const uuid =  verify(token, "kenzie", (err, decoded) => {
+            if(err){
+                return response.status(401).json({error: err.message})
+            }
+
+            return decoded.uuid
+        })
 
         next()
     }
